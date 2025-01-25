7995399c247972b80e4bd6c465f6c30666a3d7b1
YJJ <yeonjoo901@gmail.com>
2025-01-25 22:14:45 +0900

    first commit

---
+++ src/main/java/yjj/wetrash/WetrashBackendApplication.java
@@ -0,0 +1,13 @@
+package yjj.wetrash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetrashBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WetrashBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yjj/wetrash/WetrashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package yjj.wetrash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetrashBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
